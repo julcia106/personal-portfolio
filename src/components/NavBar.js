2416e8d525b6720d87f712e01fcdce5eae0e9734
Julia <juliaarutkowska@gmail.com>
2023-09-23 14:56:35 +0200

    Proper redirecting do contact page + adjustements

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,34 +8,38 @@ import navIcon2 from '../assets/img/nav-icon2.svg'
 import navIcon3 from '../assets/img/nav-icon3.svg'
 
 export const NavBar = () => {
-    const [activeLink, setActiveLink] = useState('home');
-    const [scrolled, setScrollet] = useState(false);
+  const [activeLink, setActiveLink] = useState('home');
+  const [scrolled, setScrollet] = useState(false);
 
-    // similar to "watch" in vue
-    useEffect(() => {
-        const onScroll = () => {
-            if(window.scrollY > 50){
-                setScrollet(true);
-            } else {
-                setScrollet(false);
-            }
-        }
-        window.addEventListener("scroll", onScroll);
-        return()=> window.removeEventListener("scroll", onScroll);
-    }, [])
-
-    const onUpdateActiveLink = (value) => {
-        setActiveLink(value);
+  // similar to "watch" in vue
+  useEffect(() => {
+    const onScroll = () => {
+      if (window.scrollY > 50) {
+        setScrollet(true);
+      } else {
+        setScrollet(false);
+      }
     }
+    window.addEventListener("scroll", onScroll);
+    return () => window.removeEventListener("scroll", onScroll);
+  }, [])
 
-    return (
-    <Navbar expand="lg" className= {scrolled ? "scrolled" : ""}>
+  const onUpdateActiveLink = (value) => {
+    setActiveLink(value);
+  }
+
+  const handleButtonClick = () => {
+    window.location.href = '#contact';
+  };
+
+  return (
+    <Navbar expand="lg" className={scrolled ? "scrolled" : ""}>
       <Container>
         <Navbar.Brand href="#home">
-            <img src={logo} alt="Logo" />
+          <img src={logo} alt="Logo" />
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav"> 
-         <span className="navbar-toggler-icon"></span>
+        <Navbar.Toggle aria-controls="basic-navbar-nav">
+          <span className="navbar-toggler-icon"></span>
         </Navbar.Toggle>
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
@@ -44,14 +48,13 @@ export const NavBar = () => {
             <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
           </Nav>
           <span className="navbar-text">
-          <div className="social-icon">
-            <a href= "#"><img src={navIcon1} alt="" /></a>          
-            <a href= "#"><img src={navIcon2} alt="" /></a>          
-            <a href= "#"><img src={navIcon3} alt="" /></a>          
-          </div>
-          <button className="vvd" onClick={()=> 
-            console.log('connect')}><span>Let's Connect</span>
-          </button>
+            <div className="social-icon">
+              <a href="https://www.linkedin.com/in/julia-rutkowska-184b24209" target="_blank" rel="noopener noreferrer"><img src={navIcon1} alt="LinkedIn" /></a>
+              <a href="#"><img src={navIcon2} alt="" /></a>
+              <a href="#"><img src={navIcon3} alt="" /></a>
+            </div>
+            <button className="vvd" onClick={handleButtonClick}><span>Let's Connect</span>
+            </button>
           </span>
         </Navbar.Collapse>
       </Container>
